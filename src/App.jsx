18753c28ce3d80f63d41ba5936e01287730edf0c
Tushar Anand <tushar.nasya@gmail.com>
2023-12-01 13:35:59 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ function App() {
   return (
     <>
        <h1 className="text-3xl font-bold underline">
-      Hello world!
+      Hello world! tushar anand tailwind css working 
     </h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, doloribus! Dicta maxime fugit, at velit iusto quidem inventore minus officiis. Sint minus iure placeat quisquam soluta optio id exercitationem quos veritatis eligendi vel sunt, delectus adipisci, alias dolores ipsum inventore!</p>
     </>
   )
 }
